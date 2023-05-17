56c58caffa00a2c0eb21e1c9466b39b95ae4d745
AdanSerrano <adan.serrano@utp.ac.pa>
2023-05-17 09:57:11 -0500

    feat(hero-section): :bug: data-id todo
    new functionality for list todo(checked and complete)
    
    inicialitation

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,4 +1,4 @@
-import {Todo} from '../todo/models/todo.models'
+import { Todo } from '../todo/models/todo.models'
 
 const Filters = {
     All: 'all',
@@ -10,17 +10,76 @@ const state = {
     todos: [
         new Todo('Piedra del alma'),
         new Todo('Piedra del tiempo'),
-        new Todo('Piedra del Infinito')
+        new Todo('Piedra del Infinito'),
+        new Todo('Piedra del realidad')
     ],
     filter: Filters.All,
-    
+
 }
 
 const initStore = () => {
     console.log(state);
-    console.log( 'Store init 🥑' );
+    console.log('Store init 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const getTodos = ( filter = Filters.All) => {
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed: 
+            return state.todos.filter( todo => todo.done)
+        case Filters.Pending: 
+            return state.todos.filter(todo => !todo.done)
+        default:
+            throw new Error('Option ${filter} is no valids')
+    }
+}
+
+const addTodo = (description) => {
+    if (!description) {
+        throw new Error('Description is required')
+    }
+    state.todos.push(new Todo (description))
+}
+
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map(todo => {
+        if (todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+}
+
+const deleteTodo = (todoId) => {
+    state.todos = state.todos.filter(todo => todo.id !== todoId)
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done)
+}
+
+const setFilter = (newFilter = Filers.All) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+
 export default {
-    initStore
+    initStore,
+    loadStore,
+    getTodos,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter
 }
--- src/todo/app.js
+++ src/todo/app.js
@@ -1,11 +1,48 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw';
+import { renderTodos } from './use-cases';
+
+
+
+const elementIDs = {
+    TodoList: '.todo-list',
+    NewTodoImput: '.new-todo',
+
+}
 export const App = (elementId) => {
 
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos(elementIDs.TodoList, todos)
+    }
+
     // CUANDO LA FUNCION SE LLAMA 
     (() => {
-        const app = document.createElement('div');
-        app.innerHTML = `${html}`;
-        document.querySelector(elementId).appendChild(app);
+        const app = document.createElement('section');
+        app.innerHTML = html;
+        document.querySelector(elementId).append(app);
+        displayTodos();
     })();
-};
+
+    //referencias html 
+    const newDescriptionImput = document.querySelector(elementIDs.NewTodoImput);
+    const todoListUL = document.querySelector(elementIDs.TodoList);
+
+
+
+    //listeners 
+    newDescriptionImput.addEventListener('keyup', (event) => {
+        if (event.keyCode !== 13) return;
+        if (event.target.value.trim().length === 0) return;
+        todoStore.addTodo(event.target.value)
+        displayTodos();
+        event.target.value = '';
+    });
+
+    todoListUL.addEventListener('click', (event) => {
+        const element = event.target.closest('[data-id]')
+        todoStore.toggleTodo(element.getAttribute('data-id'));
+        displayTodos();
+    });
+}
