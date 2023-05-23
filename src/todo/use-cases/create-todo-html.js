da2ff72643c7724a58a251b9d2a9a34ad2d6ffed
AdanSerrano <adan.serrano@utp.ac.pa>
2023-05-23 12:04:46 -0500

    feat(toggle todo): :bug: bug in the toggle todo

--- src/todo/use-cases/create-todo-html.js
+++ src/todo/use-cases/create-todo-html.js
@@ -1,12 +1,26 @@
 
 export const createTodoHtml = (todo) => {
-    if(!todo) {
+    if (!todo) {
         throw new Error('Invalid todo');
     }
 
-    const html = `<h1> ${todo.description} </h1>`;
+    const { done, description, id,  } = todo;
+
+    const html = `
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${ done ? 'checked' : ''}>
+                    <label>${description}</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template">
+           `;
+
     const liElement = document.createElement('li')
     liElement.innerHTML = html;
+    liElement.setAttribute('data-id', id);
+    if (todo.completed === true) {
+        liElement.classList.add('completed');       
+    }
 
     return liElement;
 }
